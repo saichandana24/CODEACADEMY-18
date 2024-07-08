e35310ff1f07dfdd97fbbc1e495e9ea16e9a420c
saichandana24 <144717457+saichandana24@users.noreply.github.com>
2024-07-08 17:32:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+function openDocument() {
+    var name = document.getElementById("search").value;
+    switch(name)
+    {
+        case "python":
+            window.location.href = "python.html"; 
+            break;
+        case "cpp":
+            window.location.href = "cpp.html"; 
+            break;
+        case "java":
+            window.location.href = "java.html"; 
+            break;
+        case "strivers":
+            window.location.href = "strivers.html"; 
+            break;
+        case "telusko":
+            window.location.href = "telusko.html"; 
+            break;
+        case "webd":
+            window.location.href = "webd.html"; 
+            break;
+        case "lovebabbar":
+            window.location.href = "lovebabbar.html"; 
+            break;
+        case "apna":
+            window.location.href = "apna.html";
+            break; 
+        case "apnacollege":
+            window.location.href = "apna.html";
+            break; 
+        case "codewithharry":
+            window.location.href = "codewithharry.html"; 
+            break;
+        default :
+            window.location.href = "main.html"; 
+    }
+    
+
+   /* if (name === "python") {
+      window.location.href = "python.html"; // Change the path accordingly
+    } 
+   else if (name === "cpp") {
+       
+      }
+     else if (name === "java") {
+        window.location.href = "java.html"; // Change the path accordingly
+      }
+     else if (name === "strivers") {
+        window.location.href = "python.html"; // Change the path accordingly
+      }
+     else if (name === allowedName) {
+        window.location.href = "python.html"; // Change the path accordingly
+      }
+     else if (name === allowedName) {
+        window.location.href = "python.html"; // Change the path accordingly
+      }
+     else if (name === allowedName) {
+        window.location.href = "python.html"; // Change the path accordingly
+      }
+      else if (name === allowedName) {
+        window.location.href = "python.html"; // Change the path accordingly
+      }
+    else {
+      alert("Sorry, you are not authorized to access this document.");
+    }*/
+  }
